0e0a9392ca6319b683e2c0f741740f634c4eda55
atif <officialarshad16@gmail.com>
2024-08-19 19:49:13 +0530

    initial-commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Compoents/Navbar";
+import Footer from "../Compoents/Footer";
+
+function Home() {
+  return (
+    <section>
+      <Navbar />
+      <Footer />
+    </section>
+  );
+}
+
+export default Home;
